f644deb306e6690f1ea01469f4f0214bb1e7c8e6
LeviOP <35459268+LeviOP@users.noreply.github.com>
2020-12-30 20:15:27 -0600

    Update dgg-emotes-latest.user.js

--- dgg-emotes-latest.user.js
+++ dgg-emotes-latest.user.js
@@ -1,25 +1,27 @@
 // ==UserScript==
 // @name         D.gg Emotes
 // @namespace    http://tampermonkey.net/
-// @version      1.2
+// @version      1.3
 // @description  Various quality of life improvements for new and old d.gg chatters
 // @author       Levi_OP
-// @match        https://www.destiny.gg/bigscreen*
+// @match        *://www.destiny.gg/bigscreen*
+// @match        *://www.destiny.gg/embed/chat*
 // @grant        none
 // ==/UserScript==
 
 (function() {
     'use strict';
-    console.log("D.gg Emotes v1.2 loaded")
-    document.getElementById("chat-wrap").children[0].contentDocument.addEventListener('click', function(e) {
+    console.log("D.gg Emotes v1.3 loaded")
+    var chat = document.getElementById("chat-wrap") ? document.getElementById("chat-wrap").children[0].contentDocument : document
+    chat.addEventListener('click', function(e) {
         e = e || window.event;
         var target = e.target || e.srcElement;
+        var input = chat.getElementById("chat-input-control")
         if (target.classList.contains("emote") && !target.parentElement.classList.contains("emote-item")) {
-            var value = document.getElementById("chat-wrap").children[0].contentDocument.getElementById("chat-input-control").value
-            if (value === "") {
-                document.getElementById("chat-wrap").children[0].contentDocument.getElementById("chat-input-control").value = target.innerText + " "
+            if (input.value === "") {
+                input.value = target.innerText + " "
             } else {
-                document.getElementById("chat-wrap").children[0].contentDocument.getElementById("chat-input-control").value += value.endsWith(" ") ? "" + target.innerText + " " : " " + target.innerText + " "
+                input.value += input.value.endsWith(" ") ? "" + target.innerText + " " : " " + target.innerText + " "
             }
         }
     }, false);

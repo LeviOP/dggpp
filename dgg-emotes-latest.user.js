c8d1cbb2a51ac415d99e9515287f961b40c30b66
LeviOP <35459268+LeviOP@users.noreply.github.com>
2020-12-30 19:50:14 -0600

    Update dgg-emotes-latest.user.js

--- dgg-emotes-latest.user.js
+++ dgg-emotes-latest.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         D.gg Emotes
 // @namespace    http://tampermonkey.net/
-// @version      1.1
+// @version      1.2
 // @description  Various quality of life improvements for new and old d.gg chatters
 // @author       Levi_OP
 // @match        https://www.destiny.gg/bigscreen*
@@ -10,11 +10,17 @@
 
 (function() {
     'use strict';
+    console.log("D.gg Emotes v1.2 loaded")
     document.getElementById("chat-wrap").children[0].contentDocument.addEventListener('click', function(e) {
         e = e || window.event;
         var target = e.target || e.srcElement;
         if (target.classList.contains("emote") && !target.parentElement.classList.contains("emote-item")) {
-            document.getElementById("chat-wrap").children[0].contentDocument.getElementById("chat-input-control").value += target.innerText + " "
+            var value = document.getElementById("chat-wrap").children[0].contentDocument.getElementById("chat-input-control").value
+            if (value === "") {
+                document.getElementById("chat-wrap").children[0].contentDocument.getElementById("chat-input-control").value = target.innerText + " "
+            } else {
+                document.getElementById("chat-wrap").children[0].contentDocument.getElementById("chat-input-control").value += value.endsWith(" ") ? "" + target.innerText + " " : " " + target.innerText + " "
+            }
         }
     }, false);
 })();

233fa8ef872da320d0e15136a6af75633a8f23c5
LeviOP <35459268+LeviOP@users.noreply.github.com>
2020-12-30 20:15:47 -0600

    Create dgg-emotes-v1.3.user.js

---
+++ versions/dgg-emotes-v1.3.user.js
@@ -0,0 +1,28 @@
+// ==UserScript==
+// @name         D.gg Emotes
+// @namespace    http://tampermonkey.net/
+// @version      1.3
+// @description  Various quality of life improvements for new and old d.gg chatters
+// @author       Levi_OP
+// @match        *://www.destiny.gg/bigscreen*
+// @match        *://www.destiny.gg/embed/chat*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    console.log("D.gg Emotes v1.3 loaded")
+    var chat = document.getElementById("chat-wrap") ? document.getElementById("chat-wrap").children[0].contentDocument : document
+    chat.addEventListener('click', function(e) {
+        e = e || window.event;
+        var target = e.target || e.srcElement;
+        var input = chat.getElementById("chat-input-control")
+        if (target.classList.contains("emote") && !target.parentElement.classList.contains("emote-item")) {
+            if (input.value === "") {
+                input.value = target.innerText + " "
+            } else {
+                input.value += input.value.endsWith(" ") ? "" + target.innerText + " " : " " + target.innerText + " "
+            }
+        }
+    }, false);
+})();
